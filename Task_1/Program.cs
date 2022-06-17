dad5d84d1198061a8a606ea33fac815521094eef
MaximBabenkov <lostbass@mail.ru>
2022-06-17 18:17:43 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать все натуральные числа от n до 1, n задано
+void PrintNumbers (int n)
+{    
+    if (n<1) return;
+    Console.Write(n+" ");   
+    PrintNumbers(n-1);
+    
+}
+Console.Clear();
+PrintNumbers(4);
